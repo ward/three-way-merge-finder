dca1c245c83537c4fb74da0525b68b0c77225bda
Ward Muylaert <ward.muylaert@gmail.com>
2020-03-12 11:35:29 +0100

    Add a library file to move the "debuggy" things into

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{App, Arg};
+use three_way_merge_finder::debugging;
 
 fn main() {
     let matches = App::new("Merge Finder")
@@ -25,11 +26,12 @@ fn main() {
         .push_head()
         .expect("Failed to set HEAD as the revwalk starting point");
     revwalk.set_sorting(git2::Sort::TOPOLOGICAL);
-    let merges = find_merges(&repo, revwalk);
-    for merge in merges {
-        println!("{},{},{},{}", merge.o, merge.a, merge.b, merge.m);
-        compare_commits(&repo, &merge);
-    }
+    debugging::diff_walk(&repo, revwalk);
+    // let merges = find_merges(&repo, revwalk);
+    // for merge in merges {
+    //     println!("{},{},{},{}", merge.o, merge.a, merge.b, merge.m);
+    //     compare_commits(&repo, &merge);
+    // }
 }
 
 fn find_merges(repo: &git2::Repository, revwalk: git2::Revwalk) -> Vec<ThreeWayMerge> {
@@ -91,36 +93,3 @@ struct ThreeWayMerge {
     b: git2::Oid,
     m: git2::Oid,
 }
-
-fn _commit_printing(repo: git2::Repository, revwalk: git2::Revwalk) {
-    for oid in revwalk {
-        let oid = oid.expect("Expected to get an object identity (oid), but it failed.");
-        let commit = repo
-            .find_commit(oid)
-            .expect("Failed to turn oid into a commit.");
-        println!(
-            "commit {:?} has {} parents",
-            commit.id(),
-            commit.parent_count()
-        );
-        for (ctr, parent) in commit.parent_ids().enumerate() {
-            println!("    Parent nr {}: {}", ctr + 1, parent);
-        }
-        // merge_base_many expects a fixed size array, not sure if I can easily go from an iterator
-        // to that. Instead just limit it to the merges with exactly two parents. This is the
-        // vaaaast majority anyway. Also the only ones I really consider.
-        if commit.parent_count() == 2 {
-            let parent1 = commit
-                .parent_id(0)
-                .expect("Failed to get id for first parent.");
-            let parent2 = commit
-                .parent_id(1)
-                .expect("Failed to get id for second parent.");
-            match repo.merge_base(parent1, parent2) {
-                Ok(oid) => println!("Base at {}", oid),
-                Err(e) => panic!("Could not find base for these parent commits. {}", e),
-            }
-        }
-    }
-}
-// The repository type has a merge_base method which finds a base for two given commits.
