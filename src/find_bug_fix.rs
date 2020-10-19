f6d69a3d6c5dfd5e780a59984101acc1ed2624ac
Ward Muylaert <ward.muylaert@gmail.com>
2020-10-19 16:04:36 +0200

    More efficient way to get descendants
    The idea I head at the end of writing the previous version was indeed
    better. Had to change it cause the current method was suuuuuuuuuper slow
    on a bigger repository.

--- src/find_bug_fix.rs
+++ src/find_bug_fix.rs
@@ -5,7 +5,6 @@ pub fn find_bug_fixing_commit(
     repo: &git2::Repository,
     ancestor_str: &str,
 ) -> Result<(), git2::Error> {
-    // TODO Not 100% sure I will need that generic revwalk.
     println!(
         "Looking for commit: {} in repo {}",
         ancestor_str,
@@ -23,40 +22,38 @@ pub fn find_bug_fixing_commit(
     Ok(())
 }
 
-/// Bit convoluted way to get all the descendants of a certain commit. Cannot just use revwalk
-/// since that only goes up the parent chain when starting at a certain commit.
 ///
-/// Possible improvement: Do topological revwalk from HEAD, but reverse it. Topological ensures
-/// that all children have been handled before a parent is handled. Thus reversed means that when
-/// you encounter a commit, all its _parents_ have already been handled. THUS if I keep a set of
-/// descendants, starting with just the commit we care about, then it is a matter of checking the
-/// parents of every commit I encounter. If a parent appears in my descendant list, add it to the
-/// descendant list. In this manner I can still Revwalk. Question: Is this better? Supposedly the
-/// revwalk first does all the parent stuff I do manually here, so that would not help much...
+/// Bit convoluted way to get all the descendants of a certain commit. Doing a reversed topological
+/// revwalk with HEAD as the start. Topological ensures all children have been handled before a
+/// parent is handled. Thus reversed means that when you encounter a commit, all its _parents_ have
+/// already been handled. THUS if I keep a set of descendants, starting with just the commit we
+/// care about, then it is a matter of checking the parents of every commit I encounter. If a
+/// parent appears in my descendant list, add it to the descendant list.
 fn get_descendants(
     repo: &git2::Repository,
     ancestor: git2::Oid,
 ) -> Result<HashSet<git2::Oid>, git2::Error> {
-    let head = repo.head()?.target().unwrap();
-
     let mut descendants = HashSet::new();
+    descendants.insert(ancestor);
 
-    // Loop through worklist, find parents for each head, add head to the children, add the combo
-    // to the worklist.
-    let mut worklist = vec![(head, vec![])];
-    while !worklist.is_empty() {
-        let (next, mut children) = worklist.pop().unwrap();
-        if next == ancestor {
-            descendants.extend(children);
-            continue;
+    let mut revwalk = repo.revwalk().unwrap();
+    revwalk.push_head().unwrap();
+    let mut sorting = git2::Sort::TOPOLOGICAL;
+    sorting.insert(git2::Sort::REVERSE);
+    revwalk.set_sorting(sorting).unwrap();
+
+    for oid in revwalk {
+        let oid = oid.unwrap();
+        let commit = repo.find_commit(oid).unwrap();
+        let parent_in_descendants = (0..commit.parent_count())
+            .map(|ctr| commit.parent_id(ctr).unwrap())
+            .any(|parent| descendants.contains(&parent));
+        if parent_in_descendants {
+            descendants.insert(oid);
         }
-        let next_c = repo.find_commit(next).unwrap();
-        children.push(next);
-        (0..next_c.parent_count())
-            .map(|ctr| next_c.parent_id(ctr).unwrap())
-            .for_each(|parent| {
-                worklist.push((parent, children.clone()));
-            });
     }
+
+    descendants.remove(&ancestor);
+
     Ok(descendants)
 }
